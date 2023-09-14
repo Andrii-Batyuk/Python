cd553d40532f42ad1a90ee50f1f0139676f4a345
andrii.batyuk@gamail.com <andrii.batyuk@gmail.com>
2023-09-14 11:44:54 +0300

    `

---
+++ Homework.py
@@ -0,0 +1,7 @@
+#Phyton Home Work  1
+color = "RED"
+age = 3
+animal = "squirrel"
+alcohol = "vodka"
+print ("When I drink", alcohol, "for", age, "monthes, I meet BIG", color, animal+ "!!!")
+print (animal)
